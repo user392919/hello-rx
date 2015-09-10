54493ab0c9f044b34e13ba151f1358e35647c04d
user392919 <user392919@gmail.com>
2015-09-10 13:12:32 +0530

    adding thread id to print in traditional method

--- src/main/java/com/example/rxjava/MyTraditional.java
+++ src/main/java/com/example/rxjava/MyTraditional.java
@@ -3,22 +3,22 @@ package com.example.rxjava;
 public class MyTraditional {
 
     public static void main(String[] args) {
-        System.out.println("getting sum ...");
+        System.out.println("getting sum ... by thread: "+ Thread.currentThread().getId() );
         int a = getSum(100);
 
-        System.out.println("processing " + a + "  .....");
+        System.out.println("processing dependent process " + a + "  ..... by thread: "+ Thread.currentThread().getId());
 
         independentProcess();
     }
 
     private static void independentProcess() {
-        System.out.println("precessing something else ....");
+        System.out.println("precessing something else .... by thread: "+ Thread.currentThread().getId());
     }
 
     private static int getSum(int i) {
 
         int tot = 0;
-
+        System.out.println("calculating sum ....  by thread: "+ Thread.currentThread().getId());
         for (int k = 0; k <= i; k++) {
             try {
                 Thread.sleep(10);
